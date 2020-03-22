393df13d07d8b7c00b83ed191cd068beab446aba
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-03-22 11:34:45 +0300

    Complete build project
    Complete project

---
+++ precache-manifest.a6c0f70b761f2b7d5b0d04f203cc9656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6703c7dd2079ab6b2e12827b1b7cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1517a8a45aae74151781",
+    "url": "/static/css/main.1d8a5fdd.chunk.css"
+  },
+  {
+    "revision": "8811e0a01cd4db1e070e",
+    "url": "/static/js/2.eda085e9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.eda085e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1517a8a45aae74151781",
+    "url": "/static/js/main.2cb7c14e.chunk.js"
+  },
+  {
+    "revision": "eb0246ce9f6fcbe1367e",
+    "url": "/static/js/runtime-main.cd0d44b5.js"
+  }
+]);
